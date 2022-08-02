ec045e4c42376d6f8796e741a6676bcb7aaefa1c
Masoud Sh <zzmasoud@gmail.com>
2022-08-02 11:28:25 +0200

    Add FeedItem model

---
+++ ZZFeed/ZZFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  ZZFeed
+//
+//  Created by Masoud on 2/8/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let imageURL: URL
+    let description: String?
+    let location: String?
+}
