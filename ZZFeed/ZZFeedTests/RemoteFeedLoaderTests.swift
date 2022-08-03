7bb48f6b01f7ae561b493ba42d6d0dedd39a9e0e
Masoud Sh <zzmasoud@gmail.com>
2022-08-03 16:00:39 +0200

    Add and done two test for HttpClient instance

--- ZZFeed/ZZFeedTests/RemoteFeedLoaderTests.swift
+++ ZZFeed/ZZFeedTests/RemoteFeedLoaderTests.swift
@@ -28,14 +28,27 @@ class RemoteFeedLoader {
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotReqDataFromURL() {
-        
+        let (client, _) = makeSUT()
+        XCTAssertNil(client.requestedURL)
     }
     
     func test_load_reqDataFromURL() {
+        let url = URL(string: "https://v1.api.com")!
+        let (client, sut) = makeSUT(url: url)
+        
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURL, url)
         
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://foo.bar")!) -> (client: TestHttpClient, sut: RemoteFeedLoader)  {
+        let client = TestHttpClient()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (client, sut)
+    }
     
     private class TestHttpClient: HttpClient {
         var requestedURL: URL?
