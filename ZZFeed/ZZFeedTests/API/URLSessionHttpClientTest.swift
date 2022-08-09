ce55de6dd5b162ec9e993e910de6bd0480b49dc6
Masoud Sh <zzmasoud@gmail.com>
2022-08-09 14:32:19 +0200

    Remove production codes from test file

--- ZZFeed/ZZFeedTests/API/URLSessionHttpClientTest.swift
+++ ZZFeed/ZZFeedTests/API/URLSessionHttpClientTest.swift
@@ -8,28 +8,6 @@
 import XCTest
 import ZZFeed
 
-class URLSessionHttpClient {
-    private let session: URLSession
-    
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    struct UnexpectedValuesRepresentation: Error {}
-    
-    func get(from url: URL, completion: @escaping (HttpClientResult) -> Void) {
-        session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success(data, response))
-            } else {
-                completion(.failure(UnexpectedValuesRepresentation()))
-            }
-        }.resume()
-    }
-}
-
 class URLSessionHttpClientTest: XCTestCase {
     
     override func setUp() {
