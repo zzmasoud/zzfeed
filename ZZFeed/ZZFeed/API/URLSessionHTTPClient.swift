ea00e409769daafa6f1a328639fc4ad98cb50c1e
Masoud Sh <zzmasoud@gmail.com>
2022-08-09 13:08:33 +0200

    Add URLSessionHTTPClient class in production

---
+++ ZZFeed/ZZFeed/API/URLSessionHTTPClient.swift
@@ -0,0 +1,30 @@
+//
+//  URLSessionHTTPClient.swift
+//  ZZFeed
+//
+//  Created by Masoud on 9/8/22.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HttpClient {
+    private let session: URLSession
+
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+
+    private struct UnexpectedValuesRepresentation: Error {}
+
+    public func get(from url: URL, completion: @escaping (HttpClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }.resume()
+    }
+}
