387d1f45953c6b7a62a8d52e7df6cd68ac9cdb2d
Masoud Sh <zzmasoud@gmail.com>
2022-08-11 13:02:46 +0200

    Remove complexity in the test by adding second expectation

--- ZZFeed/ZZFeedTests/API/URLSessionHttpClientTest.swift
+++ ZZFeed/ZZFeedTests/API/URLSessionHttpClientTest.swift
@@ -23,7 +23,6 @@ class URLSessionHttpClientTest: XCTestCase {
     func test_getFromURL_performsGETRequestWithURL() {
         let url = anyURL()
         let exp = expectation(description: "Wait for request")
-        exp.expectedFulfillmentCount = 2
         
         URLProtocolStub.observeRequests { request in
             XCTAssertEqual(request.url, url)
@@ -31,9 +30,10 @@ class URLSessionHttpClientTest: XCTestCase {
             exp.fulfill()
         }
         
-        makeSUT().get(from: url) { _ in exp.fulfill() }
+        let exp2 = expectation(description: "Wait for request completion")
+        makeSUT().get(from: url) { _ in exp2.fulfill() }
         
-        wait(for: [exp], timeout: 1.0)
+        wait(for: [exp, exp2], timeout: 1.0)
     }
     
     func test_getFromURL_failsOnRequestError() {
