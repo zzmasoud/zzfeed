8db449ca7f4f3bdaf2ff3ad2e2129c9b0277c2be
Masoud Sh <zzmasoud@gmail.com>
2022-08-21 13:09:28 +0200

    Move `FeedStore` to a new file

---
+++ ZZFeed/ZZFeed/Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  ZZFeed
+//
+//  Created by Masoud on 21/8/22.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
