3cdd9b73badfbf1d230fe9910632518012ec62df
Masoud Sh <zzmasoud@gmail.com>
2022-08-24 19:20:05 +0200

    Add `LocalFeedItem` DTO to decouple storage frameworks from `FeedItem` data model.

--- ZZFeed/ZZFeedTests/Cache/CacheFeedUseCaseTests.swift
+++ ZZFeed/ZZFeedTests/Cache/CacheFeedUseCaseTests.swift
@@ -37,14 +37,15 @@ class CacheFeedUseCaseTests: XCTestCase {
     }
     
     func test_save_requestsNewCacheInsertionWithTimestampOnSuccessfulDeletion() {
-        let items = [uniqueFeedItem(), uniqueFeedItem()]
         let timestamp = Date()
+        let items = [uniqueFeedItem(), uniqueFeedItem()]
+        let localItems = items.map { LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL)}
         let (sut, store) = makeSUT(currentDate: { timestamp })
         
         sut.save(items) { _ in }
         store.completeDeletionSuccessfully()
         
-        XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed, .insert(items, timestamp)])
+        XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed, .insert(localItems, timestamp)])
     }
     
     func test_save_failsOnDeletionError() {
@@ -163,7 +164,7 @@ class CacheFeedUseCaseTests: XCTestCase {
 class FeedStoreSpy: FeedStore {
     enum ReceivedMessage: Equatable {
         case deleteCachedFeed
-        case insert([FeedItem], Date)
+        case insert([LocalFeedItem], Date)
     }
     
     private(set) var receivedMessages = [ReceivedMessage]()
@@ -183,7 +184,7 @@ class FeedStoreSpy: FeedStore {
         deletionCompletions.first?(nil)
     }
     
-    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
+    func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
         insertionCompletions.append(completion)
         receivedMessages.append(.insert(items, timestamp))
     }
