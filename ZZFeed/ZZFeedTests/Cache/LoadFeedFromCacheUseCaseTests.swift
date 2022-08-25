4bcbd56af0ac9153f880da5052bc020cd5a36fbf
Masoud Sh <zzmasoud@gmail.com>
2022-08-25 20:45:18 +0200

    `LocalFeedLoader` doesn't message upon `init()`
    before loading the feed from the cache store

---
+++ ZZFeed/ZZFeedTests/Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -0,0 +1,30 @@
+//
+//  LoadFeedFromCacheUseCaseTests.swift
+//  ZZFeedTests
+//
+//  Created by Masoud on 25/8/22.
+//
+
+import XCTest
+import ZZFeed
+
+class LoadFeedFromCacheUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    // - MARK: Helpers
+    
+    private func makeSUT(currentDate: @escaping ()->Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, store)
+    }
+}
