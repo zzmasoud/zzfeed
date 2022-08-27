fe1291572213df4a51765dac840a1d0ec8bfd646
Masoud Sh <zzmasoud@gmail.com>
2022-08-27 14:09:53 +0200

    Replace load command completion to returrn result type (`FeedLoaderResult`)

--- ZZFeed/ZZFeed/Cache/LocalFeedLoader.swift
+++ ZZFeed/ZZFeed/Cache/LocalFeedLoader.swift
@@ -12,7 +12,8 @@ public final class LocalFeedLoader {
     private let currentDate: ()->Date
     
     public typealias SaveResult = Error?
-    
+    public typealias LoadResut  = FeedLoaderResult<Error>
+
     public init(store: FeedStore, currentDate: @escaping ()->Date) {
         self.store = store
         self.currentDate = currentDate
@@ -30,8 +31,12 @@ public final class LocalFeedLoader {
         }
     }
     
-    public func load(completion: @escaping (Error?) -> Void) {
-        store.retrieve(completion: completion)
+    public func load(completion: @escaping (LoadResut) -> Void) {
+        store.retrieve { error in
+            if let error = error {
+                completion(.failure(error))
+            }
+        }
     }
     
     private func cache(_ items: [FeedItem], with completion: @escaping (SaveResult) -> Void) {
--- ZZFeed/ZZFeedTests/Cache/LoadFeedFromCacheUseCaseTests.swift
+++ ZZFeed/ZZFeedTests/Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -30,8 +30,13 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
 
         let exp = expectation(description: "wait for completion...")
         var receivedError: Error?
-        sut.load { error in
-            receivedError = error
+        sut.load { result in
+            switch result {
+            case let .failure(error):
+                receivedError = error
+            default:
+                XCTFail("Expected failure")
+            }
             exp.fulfill()
         }
         
