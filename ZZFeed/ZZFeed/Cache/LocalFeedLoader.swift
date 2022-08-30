ad589683cc06712caa6ea354198565f8afd61410
Masoud Sh <zzmasoud@gmail.com>
2022-08-30 13:06:07 +0200

    Extract local members into properties

--- ZZFeed/ZZFeed/Cache/LocalFeedLoader.swift
+++ ZZFeed/ZZFeed/Cache/LocalFeedLoader.swift
@@ -10,6 +10,8 @@ import Foundation
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: ()->Date
+    private let calendar = Calendar.current
+    private var maxCacheAgeInDays: Int { return 7 }
     
     public typealias SaveResult = Error?
     public typealias LoadResut  = FeedLoaderResult<Error>
@@ -47,8 +49,7 @@ public final class LocalFeedLoader {
     }
     
     private func validate(_ timestamp: Date) -> Bool {
-        let calendar = Calendar.current
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else { return false }
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
         return currentDate() < maxCacheAge
     }
     
