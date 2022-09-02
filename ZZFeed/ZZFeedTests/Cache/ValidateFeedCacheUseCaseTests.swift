a9e519ecbd01ce24b1f2aa4dd13a97173d42bd77
Masoud Sh <zzmasoud@gmail.com>
2022-09-02 17:48:19 +0200

    Add new use case tests file for the validation

---
+++ ZZFeed/ZZFeedTests/Cache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,30 @@
+//
+//  ValidateFeedCacheUseCaseTests.swift
+//  ZZFeedTests
+//
+//  Created by Masoud on 2/9/22.
+//
+
+import XCTest
+import ZZFeed
+
+class ValidateFeedCacheUseCaseTests: XCTestCase {
+     
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+
+    // - MARK: Helpers
+    
+    private func makeSUT(currentDate: @escaping ()->Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, store)
+    }
+}
