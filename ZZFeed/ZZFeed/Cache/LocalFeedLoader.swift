8819d2dcec9c1b311f3244f053b0bb925bb34f53
Masoud Sh <zzmasoud@gmail.com>
2022-09-07 17:45:59 +0200

    Make the `FeedCachePolicy` with no side effects.

--- ZZFeed/ZZFeed/Cache/LocalFeedLoader.swift
+++ ZZFeed/ZZFeed/Cache/LocalFeedLoader.swift
@@ -9,27 +9,22 @@ import Foundation
 
 private final class FeedCachePolicy {
     
-    private let currentDate: ()->Date
     private let calendar = Calendar.current
-    
-    init(currentDate: @escaping ()->Date) {
-        self.currentDate = currentDate
-    }
     
     private var maxCacheAgeInDays: Int {
         return 7
     }
 
-    func validate(_ timestamp: Date) -> Bool {
+    func validate(_ timestamp: Date, against date: Date) -> Bool {
         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
-        return currentDate() < maxCacheAge
+        return date < maxCacheAge
     }
 }
 
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: ()->Date
-    private let cachePolicy: FeedCachePolicy
+    private let cachePolicy = FeedCachePolicy()
     
     public typealias SaveResult = Error?
     public typealias LoadResut  = FeedLoaderResult<Error>
@@ -37,7 +32,6 @@ public final class LocalFeedLoader {
     public init(store: FeedStore, currentDate: @escaping ()->Date) {
         self.store = store
         self.currentDate = currentDate
-        self.cachePolicy = FeedCachePolicy(currentDate: currentDate)
     }
     
     public func save(_ items: [FeedItem], completion: @escaping (SaveResult) -> Void) {
@@ -58,7 +52,7 @@ public final class LocalFeedLoader {
             case let .failure(error):
                 completion(.failure(error))
                 
-            case let .fetched(items, timestamp) where self.cachePolicy.validate(timestamp):
+            case let .fetched(items, timestamp) where self.cachePolicy.validate(timestamp, against: currentDate()):
                 completion(.success(items.toModels()))
                 
             case .fetched, .empty:
@@ -74,7 +68,7 @@ public final class LocalFeedLoader {
             case .failure:
                 self.store.deleteCachedFeed(completion: {_ in })
                 
-            case let .fetched(_, timestamp) where !self.cachePolicy.validate(timestamp):
+            case let .fetched(_, timestamp) where !self.cachePolicy.validate(timestamp, against: self.currentDate()):
                 self.store.deleteCachedFeed(completion: {_ in })
             
             case .empty, .fetched:
@@ -84,7 +78,7 @@ public final class LocalFeedLoader {
     }
         
     private func cache(_ items: [FeedItem], with completion: @escaping (SaveResult) -> Void) {
-        store.insert(items.toLocal(), timestamp: currentDate()) { [weak self] error in
+        store.insert(items.toLocal(), timestamp: self.currentDate()) { [weak self] error in
             guard let _ = self else { return }
             completion(error)
         }
