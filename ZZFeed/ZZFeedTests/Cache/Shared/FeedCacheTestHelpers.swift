cf914f5af34ce0bc064873fc15cb0224c1caa093
Masoud Sh <zzmasoud@gmail.com>
2022-09-08 20:57:41 +0200

    Hide cache expiration details from tests

---
+++ ZZFeed/ZZFeedTests/Cache/Shared/FeedCacheTestHelpers.swift
@@ -0,0 +1,17 @@
+//
+//  FeedCacheTestHelpers.swift
+//  ZZFeedTests
+//
+//  Created by Masoud Sheikh Hosseini on 9/8/22.
+//
+
+import Foundation
+
+extension Date {
+    func minusFeedCacheMaxAge() -> Date {
+        return add(days: -7)
+    }
+    func add(days: Int) -> Date {
+        return Calendar.current.date(byAdding: .day, value: -days, to: self)!
+    }
+}
