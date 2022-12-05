1dc25cd81798f057965d6bd0ec35da959d3bee96
Masoud Sh <zzmasoud@gmail.com>
2022-12-05 15:12:56 +0100

    Retrieving after inserting to empty cache delivers inserted values

--- ZZFeed/ZZFeed/Cache/LocalFeedItem.swift
+++ ZZFeed/ZZFeed/Cache/LocalFeedItem.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct LocalFeedItem: Equatable {
+public struct LocalFeedItem: Equatable, Codable {
     public let id: UUID
     public let imageURL: URL
     public let description: String?
--- ZZFeed/ZZFeedTests/Cache/CodableFeedStoreTests.swift
+++ ZZFeed/ZZFeedTests/Cache/CodableFeedStoreTests.swift
@@ -9,8 +9,28 @@ import XCTest
 import ZZFeed
 
 class CodableFeedStore {
+    private struct Cache: Codable {
+        let feed: [LocalFeedItem]
+        let timestamp: Date
+    }
+    
+    private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("feed.store")
+    
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
-        completion(.empty)
+        guard let data = try? Data(contentsOf: storeURL) else {
+            return completion(.empty)
+        }
+        
+        let decoder = JSONDecoder()
+        let decoded = try! decoder.decode(Cache.self, from: data)
+        completion(.fetched(items: decoded.feed, timestamp: decoded.timestamp))
+    }
+    
+    func insert(_ feed: [LocalFeedItem], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+        let encoder = JSONEncoder()
+        let encoded = try! encoder.encode(Cache(feed: feed, timestamp: timestamp))
+        try! encoded.write(to: storeURL)
+        completion(nil)
     }
 }
 
@@ -42,6 +62,31 @@ class CodableFeedStoreTests: XCTestCase {
                     break
                 default:
                     XCTFail("expected both empty but got \(fResult) and \(sResult)")
+                }
+                exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 1)
+    }
+    
+    func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
+        let sut = CodableFeedStore()
+        let feed = uniqueItems().local
+        let timestamp = Date()
+        let exp = expectation(description: "wait for completion...")
+        
+        sut.insert(feed, timestamp: timestamp) { insertionError in
+            XCTAssertNil(insertionError)
+            
+            sut.retrieve { retrieveResult in
+                switch retrieveResult {
+                case let .fetched(retrieveFeed, retrievedTimestamp):
+                    XCTAssertEqual(retrievedTimestamp, timestamp)
+                    XCTAssertEqual(retrieveFeed, feed)
+                    
+                default:
+                    XCTFail("expected to get .fetched but got \(retrieveResult)")
                 }
                 exp.fulfill()
             }
