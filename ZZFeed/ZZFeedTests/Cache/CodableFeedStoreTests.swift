252fb9d8dae6e8d42990b3a4b606caf0480d139f
Masoud Sh <zzmasoud@gmail.com>
2022-12-05 15:54:23 +0100

    Extract hardcoded storeURL from the `CodableFeedStore` and injected in the initializer

--- ZZFeed/ZZFeedTests/Cache/CodableFeedStoreTests.swift
+++ ZZFeed/ZZFeedTests/Cache/CodableFeedStoreTests.swift
@@ -36,7 +36,11 @@ class CodableFeedStore {
         }
     }
     
-    private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("feed.store")
+    private let storeURL: URL
+    
+    init(storeURL: URL) {
+        self.storeURL = storeURL
+    }
     
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
         guard let data = try? Data(contentsOf: storeURL) else {
@@ -135,7 +139,8 @@ class CodableFeedStoreTests: XCTestCase {
     // MARK: - Helpers
     
     func makeSUT(file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {
-        let sut = CodableFeedStore()
+        let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("feed.store")
+        let sut = CodableFeedStore(storeURL: url)
         trackForMemoryLeaks(sut)
         return sut
     }
