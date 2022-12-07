361ba54ce0e6d3fe3325b5bb728189cc2be49ef2
Masoud Sh <zzmasoud@gmail.com>
2022-12-07 13:24:18 +0100

    Replace production storeURL to avoid sharing states with other parts of the systems (e.g. other tests)

--- ZZFeed/ZZFeedTests/Cache/CodableFeedStoreTests.swift
+++ ZZFeed/ZZFeedTests/Cache/CodableFeedStoreTests.swift
@@ -143,6 +143,6 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     private func storeURL() -> URL {
-        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("feed.store")
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
     }
 }
