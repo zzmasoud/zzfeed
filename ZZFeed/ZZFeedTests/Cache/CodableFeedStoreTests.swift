d89da8374feaabd86e5ad773a2dc3d5050985fa2
Masoud Sh <zzmasoud@gmail.com>
2022-12-13 18:55:01 +0100

    Add `FeedStoreSpecs` and other protocols to simplify the module testing for clients

--- ZZFeed/ZZFeedTests/Cache/CodableFeedStoreTests.swift
+++ ZZFeed/ZZFeedTests/Cache/CodableFeedStoreTests.swift
@@ -8,6 +8,33 @@
 import XCTest
 import ZZFeed
 
+protocol FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache()
+    func test_retrieve_deliversFetchedValuesOnNonEmptyCache()
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
+    func test_retrieve_hasNoSideEffectsOnFailure()
+
+    func test_insert_overridesPreviouslyInsertedCacheValues()
+
+    func test_delete_clearsPreviouslyInsertedCache()
+
+    func test_storeSideEffects_runSerially()
+}
+
+protocol FailableRetrieveFeedStoreSpecs: FeedStoreSpecs {
+    func test_retrieve_deliversFailureOnRetrievalError()
+    func test_retrieve_hasNoSideEffectsOnEmptyCache()
+}
+
+protocol FailableInsertFeedStoreSpecs: FeedStoreSpecs {
+    func test_insert_deliversErrorOnInsertionError()
+}
+
+protocol FailableDeleteFeedStoreSpecs: FeedStoreSpecs {
+    func test_delete_deliversErrorOnDeletionError()
+    func test_delete_hasNoSideEffectsOnEmptyCache()
+}
+
 class CodableFeedStoreTests: XCTestCase {
     
     override func setUp() {
