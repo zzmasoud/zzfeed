537b973bfe292bae7b5d27ca9f241304d3e745f3
Masoud Sh <zzmasoud@gmail.com>
2022-12-25 16:54:46 +0100

    Add type aliases for `DeletionResult` and `InsertionResult`

--- ZZFeed/ZZFeed/Cache/FeedStore.swift
+++ ZZFeed/ZZFeed/Cache/FeedStore.swift
@@ -14,8 +14,11 @@ public enum CachedFeed {
 }
 
 public protocol FeedStore {
-    typealias DeletionCompletion = (Error?) -> Void
-    typealias InsertionCompletion = (Error?) -> Void
+    typealias DeletionResult = Error?
+    typealias DeletionCompletion = (DeletionResult) -> Void
+    
+    typealias InsertionResult = Error?
+    typealias InsertionCompletion = (InsertionResult) -> Void
     
     typealias RetrievalResult = Result<CachedFeed, Error>
     typealias RetrievalCompletion = (RetrievalResult) -> Void
