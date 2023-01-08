009c208013373ddaca22f8ef7f2576797d78e6d4
Masoud Sh <zzmasoud@gmail.com>
2023-01-08 09:40:50 +0100

    Simplify the closures by using an `Observer` typealias

--- ZZFeed/ZZFeediOS/Models/FeedViewModel.swift
+++ ZZFeed/ZZFeediOS/Models/FeedViewModel.swift
@@ -8,14 +8,16 @@
 import ZZFeed
 
 final class FeedViewModel {
+    typealias Observer<T> = (T) -> Void
+    
     private let feedLoader: FeedLoader
     
     public init(feedLoader: FeedLoader) {
         self.feedLoader = feedLoader
     }
 
-    var onLoadingChange: ((Bool) -> Void)?
-    var onLoad: (([FeedItem]) -> Void)?
+    var onLoadingChange: Observer<Bool>?
+    var onLoad: Observer<[FeedItem]>?
     
     func loadFeed() {
         onLoadingChange?(true)
