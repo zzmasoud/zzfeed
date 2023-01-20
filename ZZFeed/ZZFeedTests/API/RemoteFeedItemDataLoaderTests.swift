3e8127cbaf8db1cdd908e30ea04368ce1a3a351f
Masoud Sh <zzmasoud@gmail.com>
2023-01-20 16:18:40 +0100

    `RemoteFeedItemDataLoader` requests data from url on `loadImageDataFromURL`

--- ZZFeed/ZZFeedTests/API/RemoteFeedItemDataLoaderTests.swift
+++ ZZFeed/ZZFeedTests/API/RemoteFeedItemDataLoaderTests.swift
@@ -3,21 +3,46 @@
 //
 
 import XCTest
+import ZZFeed
 
 class RemoteFeedItemDataLoader {
-    init(client: Any) {}
+    private let client: HttpClient
+    
+    init(client: HttpClient) {
+        self.client = client
+    }
+    
+    func loadImageData(from url: URL) {
+        client.get(from: url) { result in
+            
+        }
+    }
 }
 
 class RemoteFeedItemDataLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestURLRequest() {
         let client = HttpClientSpy()
-        let _= RemoteFeedItemDataLoader(client: client)
+        let _ = RemoteFeedItemDataLoader(client: client)
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
-    private class HttpClientSpy {
+    func test_loadImageDataFromURL_requestDataFromURL() {
+        let client = HttpClientSpy()
+        let url = URL(string: "https://url.com")!
+        let sut = RemoteFeedItemDataLoader(client: client)
+        
+        sut.loadImageData(from: url)
+        
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    private class HttpClientSpy: HttpClient {
         var requestedURLs = [URL]()
+        
+        func get(from url: URL, completion: @escaping (HttpClient.Result) -> Void) {
+            requestedURLs.append(url)
+        }
     }
 }
