72f5dd7e16d520dd4b39b2938e0baf12ae0a9194
Masoud Sh <zzmasoud@gmail.com>
2023-01-24 13:32:44 +0100

    Refactor `switch` statement with `map` chain

--- ZZFeed/ZZFeed/API/RemoteFeedItemDataLoader.swift
+++ ZZFeed/ZZFeed/API/RemoteFeedItemDataLoader.swift
@@ -22,17 +22,12 @@ public final class RemoteFeedItemDataLoader: FeedItemDataLoader {
         task.wrapped = client.get(from: url) { [weak self] result in
             guard self != nil else { return}
             
-            switch result {
-            case .failure(let error):
-                task.complete(with: .failure(error))
-                
-            case let .success((data, response)):
-                if response.statusCode == 200, !data.isEmpty {
-                    task.complete(with: .success(data))
-                } else {
-                    task.complete(with: .failure(Error.invalidData))
-                }
-            }
+            task.complete(with: result
+                .mapError { _ in Error.connectivity }
+                .flatMap { (data, response) in
+                    let isValidResponse = response.statusCode == 200 && !data.isEmpty
+                    return isValidResponse ? .success(data) : .failure(Error.invalidData)
+                })
         }
         
         return task
