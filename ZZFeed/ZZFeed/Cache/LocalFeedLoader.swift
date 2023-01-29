a161f7ea347e2a99412ddbea74900d6484eb6a48
Masoud Sh <zzmasoud@gmail.com>
2023-01-29 09:01:14 +0100

    Move `LocalFeedLoader` save to separated extension

--- ZZFeed/ZZFeed/Cache/LocalFeedLoader.swift
+++ ZZFeed/ZZFeed/Cache/LocalFeedLoader.swift
@@ -9,26 +9,13 @@ public final class LocalFeedLoader: FeedLoader {
     private let store: FeedStore
     private let currentDate: ()->Date
     
-    public typealias SaveResult = Error?
     public typealias LoadResut  = FeedLoader.Result
 
     public init(store: FeedStore, currentDate: @escaping ()->Date) {
         self.store = store
         self.currentDate = currentDate
     }
-    
-    public func save(_ items: [FeedItem], completion: @escaping (SaveResult) -> Void) {
-        store.deleteCachedFeed { [weak self]  error in
-            guard let self = self else { return }
-            
-            if let cacheDeletionError = error {
-                completion(cacheDeletionError)
-            } else {
-                self.cache(items, with: completion)
-            }
-        }
-    }
-    
+        
     public func load(completion: @escaping (LoadResut) -> Void) {
         store.retrieve { [unowned self] result in
             switch result {
@@ -40,6 +27,24 @@ public final class LocalFeedLoader: FeedLoader {
                 
             case .success:
                 completion(.success(.empty))
+            }
+        }
+    }
+}
+
+// MARK: - Save
+
+extension LocalFeedLoader {
+    public typealias SaveResult = Error?
+
+    public func save(_ items: [FeedItem], completion: @escaping (SaveResult) -> Void) {
+        store.deleteCachedFeed { [weak self]  error in
+            guard let self = self else { return }
+            
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
+                self.cache(items, with: completion)
             }
         }
     }
