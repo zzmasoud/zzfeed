41d62068da8c141d505572589f23429121a5fc81
Masoud Sh <zzmasoud@gmail.com>
2023-02-19 12:29:12 +0100

    `CoreDataFeedStore` delivers empty on empty cache

--- ZZFeed/ZZFeedTests/Cache/CoreDataFeedStoreTests.swift
+++ ZZFeed/ZZFeedTests/Cache/CoreDataFeedStoreTests.swift
@@ -2,11 +2,29 @@
 //  Copyright © zzmasoud (github.com/zzmasoud).
 //  
 
-import Foundation
+import XCTest
+import ZZFeed
 
-class CoreDataFeedStoreTests: FeedStoreSpecs {
-    func test_retrieve_deliversEmptyOnEmptyCache() {
+class CoreDataFeedStore: FeedStore {
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         
+    }
+    
+    func insert(_ items: [ZZFeed.LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.success(.empty))
+    }
+}
+
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = CoreDataFeedStore()
+        
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
     
     func test_retrieve_deliversFetchedValuesOnNonEmptyCache() {
