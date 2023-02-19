fb333ec8c57503bd0b852b6111d30e1148593f52
Masoud Sh <zzmasoud@gmail.com>
2023-02-19 12:20:25 +0100

    Add empty CoreData feed store specs

---
+++ ZZFeed/ZZFeedTests/Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,56 @@
+//
+//  Copyright © zzmasoud (github.com/zzmasoud).
+//  
+
+import Foundation
+
+class CoreDataFeedStoreTests: FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFetchedValuesOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnFailure() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+    
+}
--- ZZFeed/ZZFeedTests/Cache/FeedStoreSpecs/FeedStoreSpecs.swift
+++ ZZFeed/ZZFeedTests/Cache/FeedStoreSpecs/FeedStoreSpecs.swift
@@ -10,10 +10,14 @@ protocol FeedStoreSpecs {
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
     func test_retrieve_hasNoSideEffectsOnFailure()
 
+    func test_insert_deliversNoErrorOnEmptyCache()
+    func test_insert_deliversNoErrorOnNonEmptyCache()
     func test_insert_overridesPreviouslyInsertedCacheValues()
 
-    func test_delete_emptiesPreviouslyInsertedCache()
+    func test_delete_deliversNoErrorOnEmptyCache()
     func test_delete_hasNoSideEffectsOnEmptyCache()
+    func test_delete_deliversNoErrorOnNonEmptyCache()
+    func test_delete_emptiesPreviouslyInsertedCache()
 
     func test_storeSideEffects_runSerially()
 }
