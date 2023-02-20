2c4be04238bf3e8936873e8d95292f2f4fe1ec3f
Masoud Sh <zzmasoud@gmail.com>
2023-02-20 19:13:28 +0100

    Decouple core data helper methods into extensions

--- ZZFeed/ZZFeed/Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ ZZFeed/ZZFeed/Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -5,11 +5,19 @@
 import CoreData
 
 public final class CoreDataFeedStore {
+    enum StoreError: Error {
+        case modelNotFound
+        case failedToLoadPersistentContainer(Error)
+    }
+    
+    private static let modelName = "FeedStore"
+    private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
+    
     private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
     
     public init(storeURL: URL, bundle: Bundle = .main) throws {
-        container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
+        container = try NSPersistentContainer.load(name: CoreDataFeedStore.modelName, model: CoreDataFeedStore.model!, url: storeURL)
         context = container.newBackgroundContext()
     }
         
