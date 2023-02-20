15e033a4a13fefaf3bc33d67a4eb073971a4d060
Masoud Sh <zzmasoud@gmail.com>
2023-02-20 14:48:27 +0100

    Move `FeedItemDataLoaderWithFallbackComposite` to production

--- ZZFeedApp/ZZFeedAppTests/FeedItemDataLoaderWithFallbackCompositeTests.swift
+++ ZZFeedApp/ZZFeedAppTests/FeedItemDataLoaderWithFallbackCompositeTests.swift
@@ -4,36 +4,7 @@
 
 import XCTest
 import ZZFeed
-
-class FeedItemDataLoaderWithFallbackComposite: FeedItemDataLoader {
-    private let primary: FeedItemDataLoader
-    private let fallback: FeedItemDataLoader
-    
-    init(primary: FeedItemDataLoader, fallback: FeedItemDataLoader) {
-        self.primary = primary
-        self.fallback = fallback
-    }
-    
-    func loadImageData(from url: URL, completion: @escaping (LoadResult) -> Void) -> FeedItemDataLoaderTask {
-        let task = TaskWrapper()
-        task.wrapped = primary.loadImageData(from: url, completion: { [weak self] result in
-            if let data = try? result.get() {
-                completion(.success(data))
-            } else {
-                task.wrapped = self?.fallback.loadImageData(from: url, completion: completion)
-            }
-        })
-        return task
-    }
-    
-    private class TaskWrapper: FeedItemDataLoaderTask {
-        var wrapped: FeedItemDataLoaderTask?
-        
-        func cancel() {
-            wrapped?.cancel()
-        }
-    }
-}
+import ZZFeedApp
 
 class FeedItemDataLoaderWithFallbackCompositeTests: XCTestCase {
     
