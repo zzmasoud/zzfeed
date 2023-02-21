56beb19848b9b155be6982d916a2d43d796f9575
Masoud Sh <zzmasoud@gmail.com>
2023-02-21 09:45:30 +0100

    Intercept HTTP requests with canned responses during UI tests to eliminate network flakiness (now it's running UI tests without internet connection)

--- ZZFeedApp/ZZFeedApp/DebuggingSceneDelegate.swift
+++ ZZFeedApp/ZZFeedApp/DebuggingSceneDelegate.swift
@@ -18,21 +18,71 @@ final class DebuggingSceneDelegate: SceneDelegate {
     }
     
     override func makeRemoteClient() -> HttpClient {
-        if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
-            return AlwaysFailingHTTPClient()
+        if let connectivity = UserDefaults.standard.string(forKey: "connectivity") {
+            return DebuggingHTTPClient(connectivity: connectivity)
         }
         return super.makeRemoteClient()
     }
 }
 
-private final class AlwaysFailingHTTPClient: HttpClient {
+private final class DebuggingHTTPClient: HttpClient {
     private class Task: HttpClientTask {
         func cancel() {}
     }
     
+    private let connectivity: String
+    
+    init(connectivity: String) {
+        self.connectivity = connectivity
+    }
+    
     func get(from url: URL, completion: @escaping (HttpClient.Result) -> Void) -> HttpClientTask {
-        completion(.failure(NSError(domain: "no connectivity", code: -1)))
+        switch connectivity {
+        case "online":
+            completion(.success(makeSuccessfullResponse(for: url)))
+        default:
+            completion(.failure(NSError(domain: "no connectivity", code: -1)))
+        }
         return Task()
+    }
+    
+    private func makeSuccessfullResponse(for url: URL) -> (Data, HTTPURLResponse) {
+        let urlResponse = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+        let data = makeData(for: url)
+        return (data, urlResponse)
+    }
+    
+    private func makeData(for url: URL) -> Data {
+        switch url.absoluteString {
+        case "https://image.com":
+            return makeImageData()
+        default:
+            return makeFeedData()
+        }
+    }
+    
+    private func makeImageData() -> Data {
+        return UIImage.make(withColor: .red).pngData()!
+    }
+    
+    private func makeFeedData() -> Data {
+        try! JSONSerialization.data(withJSONObject: ["items": [
+            ["id": UUID().uuidString, "location": "Brasil, Rio", "image": "https://image.com"],
+            ["id": UUID().uuidString, "image": "https://image.com"],
+        ]])
+    }
+}
+
+extension UIImage {
+    static func make(withColor color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1
+
+        return UIGraphicsImageRenderer(size: rect.size, format: format).image { rendererContext in
+            color.setFill()
+            rendererContext.fill(rect)
+        }
     }
 }
 #endif
