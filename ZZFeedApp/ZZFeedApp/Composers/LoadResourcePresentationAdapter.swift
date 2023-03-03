cd9842823444a320589308f921b015d41bb62968
zzmasoud <zzmasoud@gmail.com>
2023-03-03 10:54:55 +0100

    Prevent loading resources until a running request completes

--- ZZFeedApp/ZZFeedApp/Composers/LoadResourcePresentationAdapter.swift
+++ ZZFeedApp/ZZFeedApp/Composers/LoadResourcePresentationAdapter.swift
@@ -10,7 +10,7 @@ import ZZFeediOS
 final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     private let loader: () -> AnyPublisher<Resource, Error>
     private var cancellable: Cancellable?
-    
+    private var isLoading = false
     var presenter: LoadResourcePresenter<Resource, View>?
     
     init(loader: @escaping () -> AnyPublisher<Resource, Error>) {
@@ -18,11 +18,16 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     }
     
     func loadResource() {
+        guard !isLoading else { return }
         
+        isLoading = true
         presenter?.didStartLoading()
         
         cancellable = loader()
             .dispatchOnMainQueue()
+            .handleEvents(receiveCancel: { [weak self] in
+                self?.isLoading = false
+            })
             .sink(
                 receiveCompletion: { [weak self] completion in
                     switch completion {
@@ -32,6 +37,7 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
                         self?.presenter?.didFinishLoading(with: error)
                     }
                     
+                    self?.isLoading = false
                 }, receiveValue: { [weak self] resource in
                     self?.presenter?.didFinishLoading(with: resource)
                 })
