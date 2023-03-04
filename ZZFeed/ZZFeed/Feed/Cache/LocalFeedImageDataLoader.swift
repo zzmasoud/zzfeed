52695d332e82464613f5097585c0cfa87c1799a7
zzmasoud <zzmasoud@gmail.com>
2023-03-04 18:01:38 +0100

    Deprecate async APIs and replace new sync APIs

--- ZZFeed/ZZFeed/Feed/Cache/LocalFeedImageDataLoader.swift
+++ ZZFeed/ZZFeed/Feed/Cache/LocalFeedImageDataLoader.swift
@@ -7,9 +7,44 @@ import Foundation
 public protocol FeedImageDataStore {
     typealias RetrievalResult = Swift.Result<Data?, Error>
     typealias InsertionResult = Swift.Result<Void, Error>
-
+    
+    func insert(data: Data, for url: URL) throws
+    func retrieve(dataForURL url: URL) throws -> Data?
+    
+    @available(*, deprecated)
+    func insert(data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void)
+    
+    @available(*, deprecated)
     func retrieve(dataForURL url: URL, completion: @escaping (RetrievalResult) -> Void)
-    func insert(data: Data, for url: URL, completion: @escaping(InsertionResult) -> Void)
+}
+
+public extension FeedImageDataStore {
+    func insert(data: Data, for url: URL) throws {
+        let group = DispatchGroup()
+        group.enter()
+        var result: InsertionResult!
+        insert(data: data, for: url) { res in
+            result = res
+            group.leave()
+        }
+        group.wait()
+        return try result.get()
+    }
+    
+    func retrieve(dataForURL url: URL) throws -> Data? {
+        let group = DispatchGroup()
+        group.enter()
+        var result: RetrievalResult!
+        retrieve(dataForURL: url) {
+            result = $0
+            group.leave()
+        }
+        group.wait()
+        return try result.get()
+    }
+    
+    func insert(data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {}
+    func retrieve(dataForURL url: URL, completion: @escaping (RetrievalResult) -> Void) {}
 }
 
 public final class LocalFeedImageDataLoader {
